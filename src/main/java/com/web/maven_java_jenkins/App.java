806e5f45f5a5306417e15a90c687a321538329f5
PGANESH07 <sri.pganesh05@gmail.com>
2022-11-29 12:10:48 +0530

    first commit of maven java jenkins

---
+++ src/main/java/com/web/maven_java_jenkins/App.java
@@ -0,0 +1,13 @@
+package com.web.maven_java_jenkins;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World! 29 Nov 22" );
+    }
+}
